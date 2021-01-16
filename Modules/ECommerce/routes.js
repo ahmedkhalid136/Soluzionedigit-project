eb9e563c45f5e6d656a615d829f7aefd84a024ed
ahmedkhalid136 <ahmedkhalid.hta@gmail.com>
2021-01-16 13:23:15 +0500

    Routes are merged in routes.js and its working

--- Modules/ECommerce/routes.js
+++ Modules/ECommerce/routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 
 
 router.get("/ECommerce", function (req, res) {
     console.log("ECommerce Route");
+    res.send("<h1>ECommerce Route</h1>")
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(routes);
 
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World</h1>")
+});
 
 app.listen(3000, () => {
-    console.log("Server is running on Port" + PORT);
+    console.log("Server is running on Port " + PORT);
 });
