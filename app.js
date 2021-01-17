45043aa80a25cd526c2c196ba64ec7558242c7d4
Zainmir <noobcoder24>
2021-01-17 04:46:26 +0000

    app.js installing packages

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const path = require('path');
-
+const expressValidator = require('express-validator')
 const socketio = require('socket.io')
 const http = require('http')
 const app = express()
